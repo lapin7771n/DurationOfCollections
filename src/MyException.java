74b05ff82f901070e39d91f4532499dd3677305f
Nikita <nikita.lapin.201248@gmail.com>
2018-10-29 15:18:02 +0200

    Delete MyException.java

--- src/MyException.java
+++
@@ -1,8 +0,0 @@
-public class MyException extends Exception {
-    public MyException(String message) {
-        super(message);
-    }
-    public void m(){
-        System.out.println("Hi from my Exception");
-    }
-}
